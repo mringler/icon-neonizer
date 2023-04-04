65dd23fffb5c7fddea72341019b4fc82cde13cce
mringler <m@mringler.net>
2023-04-04 12:10:45 +0200

    change order of color drawer options

--- src/components/options-form/input/ColorBuilderSelect.vue
+++ src/components/options-form/input/ColorBuilderSelect.vue
@@ -18,12 +18,28 @@ const description =
     'Defines how the gradient\'s two colors are chosen from the initial color in the image. Modes distinguish between actual colors, whites and blacks, and use different mechanisms for each.'
 const colorDrawerOptions = [
     {
+        title: 'Adaptive',
+        value: ColorBuilderOption.adaptive,
+        description: 'Choose color pair build by analyzing image.',
+        colors: 'Depends on input.',
+        whites: 'Depends on input.',
+        blacks: 'Depends on input.',
+    },
+    {
         title: 'Saturate',
         value: ColorBuilderOption.saturate,
         description: 'Use bright colors based on input color.',
         colors: 'Fully saturated color and a randomly picked neighbor.',
         whites: 'Full white (#fff) and a half-saturated random color.',
         blacks: 'Full black (#000) and a slightly colored black tone.',
+    },
+    {
+        title: 'Whiteout',
+        value: ColorBuilderOption.whiteout,
+        description: 'Replace whites and blacks with randomly chosen bright colors.',
+        colors: 'Fully saturated color and a randomly picked neighbor.',
+        whites: 'Two randomly chosen bright colors.',
+        blacks: 'Two randomly chosen bright colors.',
     },
     {
         title: 'Neon',
@@ -48,22 +64,6 @@ const colorDrawerOptions = [
         colors: 'Fully saturate color and a darker shade of the color.',
         whites: 'Full white (#fff) and a half-saturated random color.',
         blacks: 'Full black (#000) and a slightly colored black tone.',
-    },
-    {
-        title: 'Whiteout',
-        value: ColorBuilderOption.whiteout,
-        description: 'Replace whites and blacks with randomly chosen bright colors.',
-        colors: 'Fully saturated color and a randomly picked neighbor.',
-        whites: 'Two randomly chosen bright colors.',
-        blacks: 'Two randomly chosen bright colors.',
-    },
-    {
-        title: 'Adaptive',
-        value: ColorBuilderOption.adaptive,
-        description: 'Choose color pair build by analyzing image.',
-        colors: 'Depends on input.',
-        whites: 'Depends on input.',
-        blacks: 'Depends on input.',
     },
 ]
 
