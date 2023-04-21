f171b3dd6626f353d5bace844012943db6d6b525
mringler <m@mringler.net>
2023-04-21 13:10:20 +0200

    added shim for .vue

--- env.d.ts
+++ env.d.ts
@@ -2,3 +2,9 @@
 
 // Vuetify color fix.
 declare module 'vuetify/lib/util/colors.mjs'
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
